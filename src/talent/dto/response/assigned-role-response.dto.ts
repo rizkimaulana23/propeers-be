860598f04f6e9ae1c15edf740b156d20519f8568
galentaris <galentaris01@gmail.com>
2025-03-17 10:45:15 +0700

    feat: add assign talent

---
+++ src/talent/dto/response/assigned-role-response.dto.ts
@@ -0,0 +1,7 @@
+export class AssignedRoleResponseDto {
+    readonly id: number;
+    readonly projectId: number;
+    readonly talentId: number;
+    readonly role: string;
+    readonly briefNotesUrl: string;
+}
